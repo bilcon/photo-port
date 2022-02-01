8ab17f27fdc77fb6164fe2cf517883f2ee1e536d
Bill Morris <bilcon@swbell.net>
2022-02-01 17:36:43 -0600

    recommit

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,10 +2,6 @@ import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
-
   const { categories = [], setCurrentCategory, currentCategory } = props;
 
   return (
